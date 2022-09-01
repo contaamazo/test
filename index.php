cbf7c9545b2dc58b26cdbe81ca6d194ac5961839
contaamazo <112597580+contaamazo@users.noreply.github.com>
2022-08-31 22:48:34 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
 
+<?php
+echo "api's";
+echo "<br>";
+echo "<a href='./apiSicaRango.php?p=XXXXXX&s=senha'> Sica - passar rango </a>";
+echo "<br>";
+echo "<a href='./apiSicaCardapio.php'> Sica - cardapio </a>";
+?>
